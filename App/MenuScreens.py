a55c021c1bfd7bb8e00482444706e2ec9d3ad5e3
Junkynioy <virgil_bravo@yahoo.com>
2022-07-21 11:21:00 +0800

    [+] Temporary, Testing, not fully functional

---
+++ App/MenuScreens.py
@@ -0,0 +1,16 @@
+from cmd import Cmd
+from App.Generic import ClearScreen
+
+## WORK IN PROGRESS, IM TRYING THIS THING OUT
+
+class Settings(Cmd):
+   def do_exit(self, inp):
+        print("Bye")
+        return True
+
+   def do_add(self, inp):
+        print("Adding '{}'".format(inp))
+
+# ClearScreen()
+Settings().cmdloop()
+print("after")
