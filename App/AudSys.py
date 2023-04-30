cc1c99f04ee690331e3ea18925770e0e9b35c2d3
Junkynioy <virgil_bravo@tahoo.com>
2023-04-30 12:34:45 +0800

    temp volume change

--- App/AudSys.py
+++ App/AudSys.py
@@ -2,7 +2,7 @@ from pygame import mixer as audio
 
 def audiosysteminit(Volume1):
     audio.init()
-    audio.music.set_volume(Volume1/1000) # Manipulated with LOAD SETTINGS such as in py soundboard
+    audio.music.set_volume(Volume1/100) # Manipulated with LOAD SETTINGS such as in py soundboard # temp change to 100
     
     # THE VOLUME @ 0.125 IS SOMEHOW LOUD. DIVIDE THE VOLUME BY 1000 (eg 100% / 1000 = 0.1)
 
@@ -23,7 +23,7 @@ class music:
         audio.music.stop()
 
     def fadeout(s):
-        audio.music.fadeout(float(s * 1000))
+        audio.music.fadeout(float(s * 100)) # temp change to 100
 
     def pause():
         audio.music.pause()
@@ -36,4 +36,4 @@ class soundfx:
     def play(filename):
         # What the actual heck,  thought you cant chain them together?????? ".play().set_volume(float)"
         # if adding pause when spamming, it's inconsistent at <0.54s delay, use >0.55 
-        audio.Sound(f".\Assets\Audio\Sfx\{filename}").play().set_volume(0.025) # again, 0.25 is somehow loud, * by 1000 (25 * 1000)
+        audio.Sound(f".\Assets\Audio\Sfx\{filename}").play().set_volume(0.25) # again, 0.25 is somehow loud, * by 1000 (25 * 1000) # temp change to 0.25 (/100)
